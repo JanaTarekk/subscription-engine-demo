62d6ca6735a955b625c69bbcbfc56f9b70d10ba1
JanaTarekk <janatarekelkott@gmail.com>
2026-06-24 21:15:25 +0300

    dashboard added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,16 @@
 import { Routes } from '@angular/router';
+
 import { PlansComponent } from './pages/plans/plans.component';
+import { SuccessComponent } from './pages/success/success.component';
+import { FailureComponent } from './pages/failure/failure.component';
+import { SubscriptionsComponent } from './pages/subscriptions/subscriptions.component';
 
 export const routes: Routes = [
-  { path: '', component: PlansComponent }
+  { path: '', component: PlansComponent },
+
+  { path: 'success', component: SuccessComponent },
+
+  { path: 'failure', component: FailureComponent },
+
+  { path: 'subscriptions', component: SubscriptionsComponent }
 ];
